5ea2d96aad8400342469f3a101ef3227991d3454
Ziyad Alotaibi <ziyad.alotaibe@gmail.com>
2022-04-18 02:08:36 +0300

    gg

--- delivery/urls.py
+++ delivery/urls.py
@@ -13,6 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
+from django.conf import settings
+
 from django.contrib import admin
 from django.urls import path,include
 from rest_framework import permissions
@@ -42,3 +45,5 @@ urlpatterns = [
 
 
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
