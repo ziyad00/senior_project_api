9941c5a3f7a32f077f87b49533388ddb953a4e5c
Ziyad Alotaibi <ziyad.alotaibe@gmail.com>
2022-04-18 01:33:43 +0300

    gg

--- carwash/admin.py
+++ carwash/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from .models import Order,Address
+from .models import Order,Address,Item,Profile
 
 
 admin.site.register(Order)
 admin.site.register(Address)
+admin.site.register(Profile)
+admin.site.register(Item)
+
 
 # Register your models here.
